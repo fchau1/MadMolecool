f8911bc51c8e56251936bf1902d7626589538a39
Ibraheem Khan <pikhan@protonmail.com>
2024-04-28 05:43:04 -0700

    fixed finetune

--- finetune_llm.py
+++ finetune_llm.py
@@ -1,18 +1,17 @@
 from transformers import GPT2LMHeadModel, GPT2Tokenizer, TextDataset, DataCollatorForLanguageModeling, Trainer, TrainingArguments
 import logging
 from transformers import logging as hf_logging
+import os
 
 # Setup logging
-logging.basicConfig(level=logging.INFO)  # Options are: DEBUG, INFO, WARNING, ERROR, and CRITICAL
+logging.basicConfig(level=logging.INFO)  # Adjust as per the desired verbosity
 hf_logging.set_verbosity_info()
 hf_logging.enable_default_handler()
 hf_logging.enable_explicit_format()
 
-# Load DistilGPT-2 model and tokenizer
 model = GPT2LMHeadModel.from_pretrained('distilgpt2')
 tokenizer = GPT2Tokenizer.from_pretrained('distilgpt2')
 
-# Load dataset
 train_dataset = TextDataset(
   tokenizer=tokenizer,
   file_path="papers_data.txt",
@@ -20,7 +19,6 @@ train_dataset = TextDataset(
 
 data_collator = DataCollatorForLanguageModeling(
   tokenizer=tokenizer, mlm=False)
-
 
 training_args = TrainingArguments(
     output_dir="./distilgpt2-finetuned-swag",
@@ -31,7 +29,7 @@ training_args = TrainingArguments(
     learning_rate=2e-4,
     save_steps=1_000,
     save_total_limit=3,
-    logging_dir='./logs',  # Save logs for monitoring the training
+    logging_dir='./logs',
     logging_steps=50,
 )
 
@@ -42,5 +40,15 @@ trainer = Trainer(
   train_dataset=train_dataset,
 )
 
-# Start training
 trainer.train()
+
+# Save model and tokenizer
+model_path = "./saved_model_directory"
+
+# Check if the directory exists, and if not, create it
+if not os.path.exists(model_path):
+    os.makedirs(model_path)
+
+
+model.save_pretrained(model_path)
+tokenizer.save_pretrained(model_path)
