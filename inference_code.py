248dfd85f8712a8ea28165b853e43195ddceebad
Ibraheem Khan <pikhan@protonmail.com>
2024-04-28 12:02:16 -0700

    openvino inference

---
+++ inference_code.py
@@ -0,0 +1,16 @@
+from openvino.inference_engine import IECore
+import torch
+
+ie = IECore()
+net = ie.read_network(model='../ovc_output/converted_model.xml', weights='../ovc_output/converted_model.bin')
+exec_net = ie.load_network(network=net, device_name="CPU")
+
+# Prepare input
+input_ids = torch.randint(0, 50256, (1, 10)).numpy()
+inputs = {"input_ids": input_ids}
+
+# Run inference
+results = exec_net.infer(inputs=inputs)
+output = results['outputs']
+
+print("Inference results:", output)
