0d414188b9853fba7e9c00dac55a88c84eac6c7f
Ibraheem Khan <pikhan@protonmail.com>
2024-04-28 12:17:11 -0700

    openvino inference

--- inference_code.py
+++ inference_code.py
@@ -7,9 +7,8 @@ ie = Core()
 # Load and compile the model for the CPU device
 compiled_model = ie.compile_model(model='../ovc_output/converted_model.xml', device_name="CPU")
 
-# Prepare input: assuming the input_ids is an integer array of token IDs
-#input_ids = np.random.randint(0, 50256, (1, 10))
-input_ids = "Could the understated effects of theobromine in coffee be responsible for putative serotonergic effects of coffee but not caffeine?"
+# Prepare input: a non tokenized example just for examples sake
+input_ids = np.random.randint(0, 50256, (1, 10))
 
 # Create a dictionary for the inputs expected by the model
 inputs = {"input_ids": input_ids}
@@ -18,7 +17,6 @@ inputs = {"input_ids": input_ids}
 result = compiled_model.create_infer_request().infer(inputs=inputs)
 
 # Access output tensor data directly from the result using the appropriate output key
-# Replace 'outputs' with the actual name of your output tensor if it's different
 output = result['outputs']
 
 print("Inference results:", output)
