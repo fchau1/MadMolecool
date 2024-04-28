ec927d9d45f86780ec99754ecc3535f74d66e54a
Ibraheem Khan <pikhan@protonmail.com>
2024-04-28 07:07:37 -0700

    test biggest dataset

--- text_gen.py
+++ text_gen.py
@@ -1,10 +1,9 @@
 import torch
-from transformers import GPT2LMHeadModel, GPT2Tokenizer
-
+from transformers import BertForMaskedLM, BertTokenizer
 
 def load_model_and_tokenizer(model_path):
-    model = GPT2LMHeadModel.from_pretrained(model_path)
-    tokenizer = GPT2Tokenizer.from_pretrained(model_path)
+    model = BertForMaskedLM.from_pretrained(model_path)
+    tokenizer = BertTokenizer.from_pretrained(model_path)
     return model, tokenizer
 
 
